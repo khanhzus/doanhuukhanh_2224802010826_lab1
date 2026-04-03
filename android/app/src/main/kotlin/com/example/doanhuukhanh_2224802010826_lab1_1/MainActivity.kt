5284eeced62b6c02ecf8801624be5da8330ab383
Doan Huu Khanh <doanhuukhanh6@gmail.com>
2026-04-03 20:43:05 +0700

    upload lab1 flutter

---
+++ android/app/src/main/kotlin/com/example/doanhuukhanh_2224802010826_lab1_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.doanhuukhanh_2224802010826_lab1_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ doanhuukhanh_2224802010826_lab1_2/test/doanhuukhanh_2224802010826_lab1_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:doanhuukhanh_2224802010826_lab1_2/doanhuukhanh_2224802010826_lab1_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
